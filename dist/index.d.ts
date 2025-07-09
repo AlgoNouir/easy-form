b62ccdd43a2d7adfe6e7416274472002f2dfd49a
Algonouir <algo.mahdi.nouri@gmail.com>
2025-07-09 14:22:03 +0330

    update build and types

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import * as _EasyForm from "./main/render";
+declare const _default: typeof _EasyForm.default;
+export default _default;
+export * from "./interfaces/index";
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import * as _EasyForm from "./main/render";
 
 export default _EasyForm.default;
-export type * from "./interfaces/index";
+export * from "./interfaces/index";
